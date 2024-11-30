687202f34794c9028421684a68798c416162f35c
Laura-C-M <shark01963@sharkcoders.pt>
2024-11-30 10:26:18 +0000

    Add files via upload

---
+++ aula_9.2.py
@@ -0,0 +1,16 @@
+peso1 = float(input("Diz-me 1 peso"))
+peso2 = float(input("Diz-me outro peso"))
+peso3 = float(input("Diz-me outro peso"))
+peso4 = float(input("Diz-me outro peso"))
+peso5 = float(input("Diz-me outro peso"))
+if peso1 > peso2 and peso1 > peso3 and peso1 > peso4 and peso1 > peso5:
+    print("O peso 1 é maior que os outros")
+elif peso2 > peso1 and peso2 > peso3 and peso2 > peso4 and peso2 > peso5:
+    print("O peso 2 é maior que os outros")
+elif peso3 > peso1 and peso3 > peso2 and peso3 > peso4 and peso3 > peso5:
+    print("O peso 3 é maior que os outros")
+elif peso4 > peso1 and peso4 > peso2 and peso4 > peso3 and peso4 > peso5:
+    print("O peso 4 é maior que os outros")
+elif peso5 > peso1 and peso5 > peso2 and peso5 > peso3 and peso5 > peso4:
+    print("O peso 5 é maior que os outros")
+#print(peso1 , peso2 , peso3 , peso4 , peso5)
---
+++ aula_9.py
@@ -0,0 +1,10 @@
+list = ['Alemanha','Áustria','Bélgica','Checoslováquia','Dinamarca','Espanha','Estônia','Finlândia','França','Grécia','Hungria', 'Itália','Letônia','Lituânia','Luxemburgo','Noruega','Países Baixos', 'Polónia', 'Protugal','Reino Unido','Roménia','Suécia','Suíça']
+pais = str(input("Este programa tem uma lista de países europeus onde não estão todos incluídos escolhe um país e descobre se ele está na lista."))
+i=0
+for i in range(len(list)-1):
+    #print(list[i])
+    pais == list[i]
+if pais == list[i]:
+    print("Certo")
+elif pais != list[i]:
+     print("Errado")
